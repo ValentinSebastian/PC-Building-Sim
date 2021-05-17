c52608806587d75e1d2cef16a8af6a1d1596ccc5
Sebi™ Sebi™ <sebytop@yahoo.com>
2021-05-17 20:45:05 +0300

    Added more assets and prepared the stage for shop

--- PC Building Sim/Assets/PickUp.cs
+++ PC Building Sim/Assets/PickUp.cs
@@ -6,7 +6,7 @@ public class PickUp : MonoBehaviour
 {
     // Start is called before the first frame update
     public Transform theDestination;
-
+    private Color startcolor;
     void OnMouseDown()
     {
         GetComponent<BoxCollider>().enabled = false;
@@ -20,5 +20,15 @@ public class PickUp : MonoBehaviour
         this.transform.parent = null;
         GetComponent<Rigidbody>().useGravity = true;
         GetComponent<BoxCollider>().enabled = true;
+    }
+
+    void OnMouseEnter()
+    {
+        startcolor = GetComponent<Renderer>().material.color;
+        GetComponent<Renderer>().material.color = Color.yellow;
+    }
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = startcolor;
     }
 }
